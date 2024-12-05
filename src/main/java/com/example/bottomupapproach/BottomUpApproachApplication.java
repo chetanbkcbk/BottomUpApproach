c49ba1fb57feddebafb37dda2ce2676dd9001d48
Chetan <chetanbkcbk@gmail.com>
2024-12-05 15:25:48 +0530

    initial commit

---
+++ src/main/java/com/example/bottomupapproach/BottomUpApproachApplication.java
@@ -0,0 +1,13 @@
+package com.example.bottomupapproach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottomUpApproachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BottomUpApproachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bottomupapproach/BottomUpApproachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bottomupapproach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BottomUpApproachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
